2c8031aad2dddde0c2a3676e79d77e864366da19
Chris Palmer <cgpalmer91@gmail.com>
2021-01-21 23:38:44 +0000

    Building a resize yuor images app.

---
+++ resize_photos.py
@@ -0,0 +1,14 @@
+# Source: https://opensource.com/life/15/2/resize-images-python
+import os
+from PIL import Image
+
+# scale by setting a height
+
+baseheight = 300
+for file in os.listdir("images/"):
+    img = Image.open(f"images/{file}")
+    hpercent = (baseheight / float(img.size[1]))
+    wsize = int((float(img.size[0]) * float(hpercent)))
+    img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+    name = file.split(".")
+    img.save(f'resized_images/{name[0]}_mobile.jpg')
