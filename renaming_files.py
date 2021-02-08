c75094f212036e834b61a3b4738e3424d9492601
Chris Palmer <cgpalmer91@gmail.com>
2021-02-08 23:23:34 +0000

    Adjusting for photos.

--- renaming_files.py
+++ renaming_files.py
@@ -2,13 +2,21 @@ import os
 import pdfplumber
 import time
 start_time = time.time()
+x = 1
+for item in os.listdir("submission_upload/"):
+      
+#     # with pdfplumber.open(f"submission_upload/{file}") as pdf:
+#     #     page_to_read = pdf.pages[0]
+#     #     extracted_text = page_to_read.extract_text()
+#     #     formatted_text = extracted_text.splitlines()
+#     #     name = formatted_text[1]
+    os.rename(f"submission_upload/{item}", f"submission_upload/{x}.jpg")
+    x = x + 1
 
-for file in os.listdir("submission_upload/"):
-    with pdfplumber.open(f"submission_upload/{file}") as pdf:
-        page_to_read = pdf.pages[0]
-        extracted_text = page_to_read.extract_text()
-        formatted_text = extracted_text.splitlines()
-        name = formatted_text[1]
-        os.rename(f"submission_upload/{file}", f"submission_upload/{name}_{file}.pdf")
+# import os
+# os.getcwd()
+# collection = "submission_upload"
+# for i, filename in enumerate(os.listdir(collection)):
+#     os.rename("submission_upload" + filename, "C:/darth_vader/" + str(i) + ".jpg")
 
-print ("My program took", time.time() - start_time, "to run")
+# print("My program took", time.time() - start_time, "to run")
