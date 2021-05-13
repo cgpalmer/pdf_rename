5c236a2c74470008bda4449d2811b3497325995c
Chris Palmer <cgpalmer91@gmail.com>
2021-05-13 19:24:59 +0000

    pushing everything.

--- resize_photos.py
+++ resize_photos.py
@@ -4,11 +4,20 @@ from PIL import Image
 
 # scale by setting a height
 
-baseheight = 300
+# baseheight = 300
+# for file in os.listdir("images/"):
+#     img = Image.open(f"images/{file}")
+#     hpercent = (baseheight / float(img.size[1]))
+#     wsize = int((float(img.size[0]) * float(hpercent)))
+#     img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+#     name = file.split(".")
+#     img.save(f'resized_images/{name[0]}_mobile.jpg')
+
+basewidth = 1920
 for file in os.listdir("images/"):
     img = Image.open(f"images/{file}")
-    hpercent = (baseheight / float(img.size[1]))
-    wsize = int((float(img.size[0]) * float(hpercent)))
-    img = img.resize((wsize, baseheight), Image.ANTIALIAS)
+    wpercent = (basewidth / float(img.size[0]))
+    hsize = int((float(img.size[1]) * float(wpercent)))
+    img = img.resize((basewidth, hsize), Image.ANTIALIAS)
     name = file.split(".")
     img.save(f'resized_images/{name[0]}_mobile.jpg')
